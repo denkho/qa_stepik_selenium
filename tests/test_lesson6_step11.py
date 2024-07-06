ac4e7bcfbf9cf176c98c25243b59b51f37160acd
den_kho <20597299+denkho@users.noreply.github.com>
2024-07-06 14:19:17 +0300

    lesson6 last steps

---
+++ tests/test_lesson6_step11.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    # Первая страница
+    # link = "http://suninjuly.github.io/registration1.html"
+
+    # Второй вариант страницы с багом
+    link = "http://suninjuly.github.io/registration2.html"
+
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Два варианта кода, который заполняет обязательные поля
+    
+    # Первый вариант
+    # browser.find_element(By.XPATH, "//input[contains(@placeholder, 'first name')]").send_keys('Mark')
+    # browser.find_element(By.XPATH, "//input[contains(@placeholder, 'last name')]").send_keys('Swen')
+    # browser.find_element(By.XPATH, "//input[contains(@placeholder, 'email')]").send_keys('mark@mail.ru')
+
+    # Второй вариант
+    browser.find_element(By.XPATH, "//label[text()='First name*']/following-sibling::input").send_keys('Mark')
+    browser.find_element(By.XPATH, "//label[text()='Last name*']/following-sibling::input").send_keys('Swen')
+    browser.find_element(By.XPATH, "//label[text()='Email*']/following-sibling::input").send_keys('mark@mail.ru')
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
