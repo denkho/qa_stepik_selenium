6d8a82e6863886a58c0c41d4d63f611957aa4027
den_kho <20597299+denkho@users.noreply.github.com>
2024-07-18 11:23:01 +0300

    test solution to check

--- tests/section_03/test_lesson_3_6_step10/test_items.py
+++ tests/section_03/test_lesson_3_6_step10/test_items.py
@@ -2,7 +2,7 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_items(browser):
+def test_button_add_to_cart_presence(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
     browser.get(link)
